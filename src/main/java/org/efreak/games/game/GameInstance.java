3e1f1c56492da960a50bad6be6a141e6f2ea560a
efreak1996 <maxjoehnk@gmail.com>
2013-04-28 21:33:43 +0200

    Added validation check for Player join and leave

--- src/main/java/org/efreak/games/game/GameInstance.java
+++ src/main/java/org/efreak/games/game/GameInstance.java
@@ -4,11 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.efreak.games.GamePlayer;
+import org.efreak.games.game.actions.PlayerJoinAction;
+import org.efreak.games.game.actions.PlayerLeaveAction;
+import org.efreak.games.game.rules.GameRule;
 
 public abstract class GameInstance {
 
 	private final Game game;
-	private List<GamePlayer> players;
+	private final List<GamePlayer> players;
 	
 	public GameInstance(Game game) {
 		this.game = game;
@@ -23,6 +26,22 @@ public abstract class GameInstance {
 	}
 	
 	public boolean join(GamePlayer player) {
+		for (GameRule rule : game.getRules()) {
+			if (!rule.isActionValid(this, new PlayerJoinAction())) return false;
+		}
+		players.add(player);
 		return true;
+	}
+	
+	public boolean leave(GamePlayer player) {
+		for (GameRule rule : game.getRules()) {
+			if (!rule.isActionValid(this, new PlayerLeaveAction())) return false;
+		}
+		players.remove(player);
+		return true;
+	}
+	
+	public List<GamePlayer> getPlayers() {
+		return players;
 	}
 }
