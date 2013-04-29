33ebddf2379253daa7843e4e4af2c6b72500994a
e-freak <maxjoehnk@gmail.com>
2013-04-29 12:51:30 +0200

    Added functions for Scoreboard API and Lobbies

--- src/main/java/org/efreak/games/GamePlayer.java
+++ src/main/java/org/efreak/games/GamePlayer.java
@@ -1,12 +1,18 @@
 package org.efreak.games;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
 import org.efreak.games.game.GameInstance;
+import org.efreak.games.game.GameLobby;
 
 public class GamePlayer {
 
 	private boolean inGame = false;
+	private boolean inLobby = false;
 	private GameInstance game = null;
+	private GameLobby lobby = null;
 	private final Player player;
 	
 	public GamePlayer(Player player) {
@@ -17,12 +23,49 @@ public class GamePlayer {
 		return inGame;
 	}
 	
+	public boolean isInLobby() {
+		return inLobby;
+	}
+	
+	public void setGame(GameInstance game) {
+		this.game = game;
+		if (game != null) inGame = true;
+		else inGame = false;
+	}
+	
 	public GameInstance getGame() {
 		return game;
+	}
+	
+	public void setLobby(GameLobby lobby) {
+		this.lobby = lobby;
+		if (lobby != null) inLobby = true;
+		else inLobby = false;
+	}
+	
+	public GameLobby getLobby() {
+		return lobby;
 	}
 	
 	public Player getPlayer() {
 		return player;
 	}
 	
+	public void setTeam(Team team) {
+		Team oldTeam = getTeam();
+		if (oldTeam != null) oldTeam.removePlayer(player);
+		team.addPlayer(player);
+	}
+	
+	public Team getTeam() {
+		return player.getScoreboard().getPlayerTeam(player);
+	}
+	
+	public void setScoreboard(Scoreboard scoreboard) {
+		player.setScoreboard(scoreboard);
+	}
+	
+	public Scoreboard getScoreboard() {
+		return player.getScoreboard();
+	}
 }
