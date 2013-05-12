fa8b206c6e764bbcf21c1886427af85fcfa557f1
e-freak <maxjoehnk@gmail.com>
2013-05-12 13:00:50 +0200

    Added createWave static Methods

--- src/main/java/org/efreak/games/game/waves/Wave.java
+++ src/main/java/org/efreak/games/game/waves/Wave.java
@@ -3,7 +3,9 @@ package org.efreak.games.game.waves;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.EntityType;
 
 public class Wave {
@@ -11,19 +13,37 @@ public class Wave {
 	private Location spawnPoint;
 	private List<EntityType> mobs;
 	
-	public Wave(int waveStrength, Location spawnPoint) {
-		mobs = new ArrayList<EntityType>();
-		//TODO: Add random mob generation based on point system
-		this.spawnPoint = spawnPoint;
-	}
-	
 	public Wave(List<EntityType> mobs, Location spawnPoint) {
 		this.mobs = mobs;
 		this.spawnPoint = spawnPoint;
 	}
-	
+
 	public void spawn() {
 		for (EntityType entity : mobs) spawnPoint.getWorld().spawnEntity(spawnPoint, entity);
+	}
+	
+	public static Wave createWave(ConfigurationSection path, ConfigurationSection spawnPath) {
+		Location location = new Location(Bukkit.getWorld(spawnPath.getString("WaveSpawn.World")), spawnPath.getDouble("WaveSpawn.X"), spawnPath.getDouble("WaveSpawn.Y"), spawnPath.getDouble("WaveSpawn.Z"));
+		return createWave(path, location);
+	}
+	
+	public static Wave createWave(ConfigurationSection path, Location location) {
+		WaveType type = WaveType.valueOf(path.getString("Type"));
+		List<EntityType> mobs = new ArrayList<EntityType>();
+		switch (type) {
+			case RANDOM:
+				
+				//TODO: Add random mob generation based on point system
+				return new Wave(mobs, location);
+			case SET:
+				for (String mob : path.getConfigurationSection("Mobs").getKeys(false)) {
+					if (EntityType.valueOf(mob.toUpperCase()) == null) continue;
+					int amount = path.getInt("Mobs." + mob);
+					for (int i = 0; i < amount; i++) mobs.add(EntityType.valueOf(mob.toUpperCase()));
+				}
+				return new Wave(mobs, location);
+		}
+		return null;
 	}
 	
 }
