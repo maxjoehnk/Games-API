9b2015fc33fc5c6e5a4d16ec2425262aa08a35fa
e-freak <maxjoehnk@gmail.com>
2013-05-12 13:06:21 +0200

    Changed GameRule type to abstract class and added name field

--- src/main/java/org/efreak/games/game/rules/GameRule.java
+++ src/main/java/org/efreak/games/game/rules/GameRule.java
@@ -3,9 +3,18 @@ package org.efreak.games.game.rules;
 import org.efreak.games.game.GameInstance;
 import org.efreak.games.game.actions.GameAction;
 
-public interface GameRule {
-
-	public boolean isValid(GameInstance instance);
-	public boolean isActionValid(GameInstance instance, GameAction action);
+public abstract class GameRule {
+		
+	protected final String name;
 	
+	public GameRule(String name) {
+		this.name = name;
+	}
+	
+	public abstract boolean isValid(GameInstance instance);
+	public abstract boolean isActionValid(GameInstance instance, GameAction action);
+	
+	public String getName() {
+		return name;
+	}
 }
