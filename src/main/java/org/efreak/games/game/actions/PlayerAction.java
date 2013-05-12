db634db1d3364a7efee85406c5e9908446035e1e
e-freak <maxjoehnk@gmail.com>
2013-05-12 13:03:22 +0200

    Changed PlayerAction Type to interface for multi inheritance

--- src/main/java/org/efreak/games/game/actions/PlayerAction.java
+++ src/main/java/org/efreak/games/game/actions/PlayerAction.java
@@ -2,16 +2,8 @@ package org.efreak.games.game.actions;
 
 import org.efreak.games.GamePlayer;
 
-public class PlayerAction implements GameAction {
-
-	private GamePlayer player;
+public interface PlayerAction extends GameAction {
 	
-	public PlayerAction(GamePlayer player) {
-		this.player = player;
-	}
-	
-	public GamePlayer getPlayer() {
-		return player;
-	}
+	public GamePlayer getPlayer();
 	
 }
