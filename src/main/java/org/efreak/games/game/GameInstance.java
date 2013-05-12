6edc5e72dc592976b3cc65300e7a30c040926d67
e-freak <maxjoehnk@gmail.com>
2013-05-12 13:08:42 +0200

    Switched to RulesList instead of List<GameRule>, Added basic config loading, added player.leaveGame() and player.setLobby() calls

--- src/main/java/org/efreak/games/game/GameInstance.java
+++ src/main/java/org/efreak/games/game/GameInstance.java
@@ -8,16 +8,21 @@ import org.efreak.games.GamePlayer;
 import org.efreak.games.Games;
 import org.efreak.games.game.actions.PlayerJoinAction;
 import org.efreak.games.game.actions.PlayerLeaveAction;
+import org.efreak.games.game.rules.ClassRule;
 import org.efreak.games.game.rules.GameRule;
+import org.efreak.games.game.rules.MaxPlayerRule;
+import org.efreak.games.game.rules.MinPlayerRule;
+import org.efreak.games.game.rules.RulesList;
 
 public abstract class GameInstance {
 
 	private static Configuration config;
 	
 	private boolean isRunning = false;
+	private boolean lobbyEnabled = true;
 	private final Game game;
 	private final List<GamePlayer> players;
-	private final List<GameRule> rules;
+	private final RulesList rules;
 	private GameLobby lobby;
 	private String id, configPath;
 	
@@ -25,14 +30,29 @@ public abstract class GameInstance {
 		config = Games.getConfiguration();
 	}
 	
-	public GameInstance(Game game, String id) {
+	public GameInstance(Game game, String id, boolean setupConfig) {
 		this.game = game;
 		players = new ArrayList<GamePlayer>();
-		rules = new ArrayList<GameRule>();
+		rules = new RulesList();
 		rules.addAll(game.getRules());
 		this.id = id;
 		configPath = "Games." + game.getName() + ".Instances." + id + ".";
-		
+		if (setupConfig) {
+			if (config.contains(configPath + "Lobby.Enabled"))
+				lobbyEnabled = config.getBoolean(configPath + "Lobby.Enabled");
+			if (config.contains(configPath + "Classes")) {
+				List<PlayerClass> classes = new ArrayList<PlayerClass>();
+				for (String className : config.getChilds(configPath + "Classes")) {
+					classes.add(new PlayerClass(configPath + "Classes." + className));
+				}
+				rules.replace(new ClassRule(classes));
+			}
+			if (config.contains(configPath + "Players.Max"))
+				rules.replace(new MaxPlayerRule(config.getInt(configPath + "Players.Max")));
+			if (config.contains(configPath + "Players.Min")) {
+				rules.replace(new MinPlayerRule(config.getInt(configPath + "Players.Min")));
+			}
+		}
 	}
 	
 	public abstract boolean start();
@@ -52,6 +72,7 @@ public abstract class GameInstance {
 		}
 		players.add(player);
 		player.setGame(this);
+		if (lobbyEnabled) player.setLobby(lobby);
 		return true;
 	}
 	
@@ -59,6 +80,7 @@ public abstract class GameInstance {
 		for (GameRule rule : rules) {
 			if (!rule.isActionValid(this, new PlayerLeaveAction(player))) return false;
 		}
+		player.leaveGame();
 		players.remove(player);
 		return true;
 	}
