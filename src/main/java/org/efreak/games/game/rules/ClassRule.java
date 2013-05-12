1e2fb054a16ebd1c2a25326614b32d6639fce4c6
e-freak <maxjoehnk@gmail.com>
2013-05-12 13:05:49 +0200

    Added isValid and isActionValid checks

--- src/main/java/org/efreak/games/game/rules/ClassRule.java
+++ src/main/java/org/efreak/games/game/rules/ClassRule.java
@@ -2,25 +2,33 @@ package org.efreak.games.game.rules;
 
 import java.util.List;
 
+import org.efreak.games.GamePlayer;
 import org.efreak.games.game.GameInstance;
 import org.efreak.games.game.PlayerClass;
+import org.efreak.games.game.actions.ClassPickAction;
 import org.efreak.games.game.actions.GameAction;
 
-public class ClassRule implements GameRule {
+public class ClassRule extends GameRule {
 
-	List<PlayerClass> classes;
+	private List<PlayerClass> classes;
 	
 	public ClassRule(List<PlayerClass> allowedClasses) {
+		super("ClassRule");
 		classes = allowedClasses;
 	}
 	
 	@Override
 	public boolean isValid(GameInstance instance) {
-		return false;
+		for (GamePlayer player : instance.getPlayers()) {
+			if (player.getPlayerClass() == null) return false;
+			if (!classes.contains(player.getPlayerClass())) return false;
+		}
+		return true;
 	}
 
 	@Override
 	public boolean isActionValid(GameInstance instance, GameAction action) {
-		return false;
+		if (action instanceof ClassPickAction) return classes.contains(((ClassPickAction) action).getPlayerClass());
+		return true;
 	}
 }
