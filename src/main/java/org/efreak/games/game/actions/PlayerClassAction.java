89a2b5665fef94ca9d1cdd47fb0a53f16cd731e1
e-freak <maxjoehnk@gmail.com>
2013-05-12 13:10:07 +0200

    Added PlayerClassAction interface for actions which do something with classes

---
+++ src/main/java/org/efreak/games/game/actions/PlayerClassAction.java
@@ -0,0 +1,9 @@
+package org.efreak.games.game.actions;
+
+import org.efreak.games.game.PlayerClass;
+
+public interface PlayerClassAction extends GameAction {
+
+	public PlayerClass getPlayerClass();
+	
+}
