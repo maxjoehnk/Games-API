01f803d934cf1aed60a0a20a38983c3c7ee024d7
e-freak <maxjoehnk@gmail.com>
2013-05-12 13:04:19 +0200

    Added missing getPlayer() and getSpawnPoint() methods

--- src/main/java/org/efreak/games/game/actions/PlayerSpawnAction.java
+++ src/main/java/org/efreak/games/game/actions/PlayerSpawnAction.java
@@ -1,11 +1,24 @@
 package org.efreak.games.game.actions;
 
+import org.bukkit.Location;
 import org.efreak.games.GamePlayer;
 
-public class PlayerSpawnAction extends PlayerAction {
+public class PlayerSpawnAction implements PlayerAction {
 
-	public PlayerSpawnAction(GamePlayer player) {
-		super(player);
+	private GamePlayer player;
+	private Location spawnPoint;
+	
+	public PlayerSpawnAction(GamePlayer player, Location spawnPoint) {
+		this.player = player;
+		this.spawnPoint = spawnPoint;
 	}
 
+	@Override
+	public GamePlayer getPlayer() {
+		return player;
+	}
+	
+	public Location getSpawnPoint() {
+		return spawnPoint;
+	}
 }
