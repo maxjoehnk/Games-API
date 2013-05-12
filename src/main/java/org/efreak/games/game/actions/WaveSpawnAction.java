9c6ad0be5b047e240687c2985779bd40664dbdfd
e-freak <maxjoehnk@gmail.com>
2013-05-12 13:05:25 +0200

    Added missing methods

--- src/main/java/org/efreak/games/game/actions/WaveSpawnAction.java
+++ src/main/java/org/efreak/games/game/actions/WaveSpawnAction.java
@@ -2,10 +2,29 @@ package org.efreak.games.game.actions;
 
 import org.efreak.games.game.waves.Wave;
 
-public class WaveSpawnAction extends WaveAction {
+public class WaveSpawnAction implements WaveAction {
 
+	private Wave wave;
+	private boolean bossWave;
+	
 	public WaveSpawnAction(Wave wave) {
-		super(wave);
+		this.wave = wave;
+		bossWave = false;
+	}
+	
+	public WaveSpawnAction(Wave wave, boolean bossWave) {
+		this.wave = wave;
+		this.bossWave = bossWave;
+	}
+	
+	@Override
+	public Wave getWave() {
+		return wave;
+	}
+
+	@Override
+	public boolean isBossWave() {
+		return bossWave;
 	}
 
 }
