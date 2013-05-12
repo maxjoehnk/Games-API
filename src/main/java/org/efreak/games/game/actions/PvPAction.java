e608e6c852a55099dafe79a9291b3633a9a64ab4
e-freak <maxjoehnk@gmail.com>
2013-05-12 13:13:46 +0200

    Added PvPAction

--- src/main/java/org/efreak/games/game/actions/PvPAction.java
+++ src/main/java/org/efreak/games/game/actions/PvPAction.java
@@ -2,10 +2,18 @@ package org.efreak.games.game.actions;
 
 import org.efreak.games.GamePlayer;
 
-public class PvPAction extends PlayerAction {
-
-	public PvPAction(GamePlayer player) {
-		super(player);
+public class PvPAction implements MultiPlayerAction {
+	
+	private GamePlayer[] players;
+	
+	public PvPAction(GamePlayer player1, GamePlayer player2) {
+		players = new GamePlayer[2];
+		players[0] = player1;
+		players[1] = player2;
 	}
 
+	@Override
+	public GamePlayer[] getPlayers() {
+		return players;
+	}
 }
