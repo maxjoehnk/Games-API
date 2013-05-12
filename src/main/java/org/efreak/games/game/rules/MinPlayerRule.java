ae74ccf42ab20f49777f46d8dcb17173cb5f30f4
e-freak <maxjoehnk@gmail.com>
2013-05-12 13:06:52 +0200

    Changed return value of isActionValid to true

--- src/main/java/org/efreak/games/game/rules/MinPlayerRule.java
+++ src/main/java/org/efreak/games/game/rules/MinPlayerRule.java
@@ -3,11 +3,12 @@ package org.efreak.games.game.rules;
 import org.efreak.games.game.GameInstance;
 import org.efreak.games.game.actions.GameAction;
 
-public class MinPlayerRule implements GameRule {
+public class MinPlayerRule extends GameRule {
 
 	private int count;
 	
 	public MinPlayerRule(int count) {
+		super("MinPlayerRule");
 		this.count = count;
 	}
 	
@@ -19,7 +20,7 @@ public class MinPlayerRule implements GameRule {
 
 	@Override
 	public boolean isActionValid(GameInstance instance, GameAction action) {
-		return false;
+		return true;
 	}
 	
 }
