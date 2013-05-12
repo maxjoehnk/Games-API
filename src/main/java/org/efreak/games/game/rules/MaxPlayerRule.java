43f9d23c67af48442008943485af6a5b8a0edf5f
e-freak <maxjoehnk@gmail.com>
2013-05-12 13:01:14 +0200

    Added isActionValid check for PlayerJoinAction

--- src/main/java/org/efreak/games/game/rules/MaxPlayerRule.java
+++ src/main/java/org/efreak/games/game/rules/MaxPlayerRule.java
@@ -2,12 +2,14 @@ package org.efreak.games.game.rules;
 
 import org.efreak.games.game.GameInstance;
 import org.efreak.games.game.actions.GameAction;
+import org.efreak.games.game.actions.PlayerJoinAction;
 
-public class MaxPlayerRule implements GameRule {
+public class MaxPlayerRule extends GameRule {
 
 	private int count;
 	
 	public MaxPlayerRule(int count) {
+		super("MaxPlayerRule");
 		this.count = count;
 	}
 	
@@ -19,7 +21,8 @@ public class MaxPlayerRule implements GameRule {
 
 	@Override
 	public boolean isActionValid(GameInstance instance, GameAction action) {
-		return false;
+		if (action instanceof PlayerJoinAction) return instance.getPlayers().size() + 1 <= count;
+		return true;
 	}
 
 	
